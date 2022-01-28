560d53e21cc075c05244cdf2010699650fe5118c
dhruvmalik007 <dhruv.malik@polytechnique.edu>
2022-01-28 11:11:51 +0100

    reverting previous change of internal bond structure

--- contracts/util/ERC3475data.sol
+++ contracts/util/ERC3475data.sol
@@ -5,12 +5,12 @@ pragma solidity ^0.6.8;
 
 contract ERC3475data {
 
-    mapping(address => mapping(uint256 => mapping(uint256 => uint256))) public _balances;
-    mapping(uint256 => mapping(uint256 => uint256)) public _activeSupply;
-    mapping(uint256 => mapping(uint256 => uint256)) public _burnedSupply;
-    mapping(uint256 => mapping(uint256 => uint256)) public _redeemedSupply;
-    mapping(uint256 => mapping(uint256 => mapping(uint256 => uint256))) public _nonceInfo;
-    mapping (uint256 => address) public _bankAddress;
+    mapping(address => mapping(uint256 => mapping(uint256 => uint256))) private _balances;
+    mapping(uint256 => mapping(uint256 => uint256)) private _activeSupply;
+    mapping(uint256 => mapping(uint256 => uint256)) private _burnedSupply;
+    mapping(uint256 => mapping(uint256 => uint256)) private _redeemedSupply;
+    mapping(uint256 => mapping(uint256 => mapping(uint256 => uint256))) private _nonceInfo;
+    mapping (uint256 => address) private _bankAddress;
     mapping (uint256 => string) public _Symbol;
     mapping (uint256 => uint256)  public last_bond_nonce;
     mapping (uint256 => uint256[]) public _nonceCreated;
