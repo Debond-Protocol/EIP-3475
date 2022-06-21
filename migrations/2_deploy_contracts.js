9ef9281588240fa1d06640d38cede37b6d847a1c
dhruvmalik007 <dhruv.malik@polytechnique.edu>
2022-06-21 16:56:44 +0200

    minor: only deployer init

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 const ERC3475 = artifacts.require("ERC3475");
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer, accounts) {
     deployer.deploy(ERC3475);
     const bond = ERC3475.deployed();
-    await bond.init();
+    await bond.init({from:accounts[0]});
 
 };
