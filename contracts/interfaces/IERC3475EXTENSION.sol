63f9d0a2ec24dca5d8c3c48cf808436c646e63b9
yuliu-debond <79855548+yuliu-debond@users.noreply.github.com>
2023-03-02 12:37:57 +0100

    Update and rename contracts/IERC3475EXTENSION.sol to contracts/interfaces/IERC3475EXTENSION.sol

--- contracts/interfaces/IERC3475EXTENSION.sol
+++ contracts/interfaces/IERC3475EXTENSION.sol
@@ -3,9 +3,9 @@
 
 pragma solidity ^0.8.0;
 
+import "./IERC3475.sol";
 
 interface IERC3475EXTENSION {
-    use IERC3475EXTENSION for IERC3475
     // STRUCTURE
     /**
      * @dev Values structure of the Metadata
