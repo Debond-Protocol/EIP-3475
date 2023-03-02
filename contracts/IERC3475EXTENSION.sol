b50ed32352ad8da80e1d514eaa6442a9f8ef8c6a
yuliu-debond <79855548+yuliu-debond@users.noreply.github.com>
2023-03-02 11:28:10 +0100

    Create IERC3475EXTENSION.sol

---
+++ contracts/IERC3475EXTENSION.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: CC0-1.0
+
+
+pragma solidity ^0.8.0;
+
+
+interface IERC3475EXTENSION {
+    use IERC3475EXTENSION for IERC3475
+    // STRUCTURE
+    /**
+     * @dev Values structure of the Metadata
+     */
+    struct Values {
+        string stringValue;
+        uint uintValue;
+        address addressValue;
+        bool boolValue;
+        string[] stringArrayValue;
+        uint[] uintArrayValue;
+        address[] addressArrayValue;
+        bool[] boolAraryValue;
+    }
+    
+       /**
+     * @dev Returns the values of the given _metadataTitle.
+     * the metadata SHOULD follow a set of structures explained in eip-3475.md
+     */
+    function classValuesFromTitle(uint256 _classId, string memory _metadataTitle) external view returns (Values memory);
+
+    /**
+     * @dev Returns the values of given _metadataTitle.
+     * @param _classId is the class of bonds for which you determine the nonce .
+     * @param _nonceId is the nonce for which you return the value struct info
+     */
+    function nonceValues(uint256 _classId, uint256 _nonceId, string memory _metadataTitle) external view returns (Values memory);    
+    
+      /**
+     * @notice MUST trigger when token class is created
+     */     
+    event classCreated(address indexed _operator, uint256 _classId);  
+  
+}
