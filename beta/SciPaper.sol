e9ce559b7cd62eb7f66516564405f20c82c56c26
yuliu-debond <79855548+yuliu-debond@users.noreply.github.com>
2023-04-25 17:09:06 +0200

    Update SciPaper.sol

--- beta/SciPaper.sol
+++ beta/SciPaper.sol
@@ -296,8 +296,6 @@ contract ERC3475 is IERC3475, IERC3475EXTENSION {
      * to be deployed during the initial deployment cycle
      */
     constructor() { 
-        _classes[1]._valuesId[1] = "symbol";    
-        _classes[1]._values["symbol"].stringValue = "desmo labs token";    
         }
         
     // WRITABLES
@@ -705,6 +703,9 @@ contract Token is ERC3475 {
     constructor() {
         publisher = msg.sender;
         _classes[0]._values["nonceProprity"].stringValue = "{0: ownership, 1: manuscript access, 2: data access, 3: algorithm access}";
+        _classes[0]._values["category"].stringValue = "Proprity";
+        _classes[0]._values["subcategory"].stringValue = "intellectualProprity";
+        _classes[0]._values["childCategory"].stringValue = "scientificResearch";
     }
 
     function _issueToken(
